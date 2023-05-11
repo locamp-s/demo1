6fe01231e4c09b3d67c523a9e15987326b727bed
Aleksandr <AVShumilin@alfabank.ru>
2023-05-11 11:52:01 +0300

    add junit tests

--- src/test/java/com/example/demo1/service/MainServiceTest.java
+++ src/test/java/com/example/demo1/service/MainServiceTest.java
@@ -2,33 +2,34 @@ package com.example.demo1.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class MainServiceTest {
+
+    @Autowired
+    private MainService mainService;
+
+    @MockBean
+    private SecondService secondService;
 
     @Test
     void sayHello() {
-        SecondService secondServiceMock = mock(SecondService.class);
-
-        MainService mainService = new MainService(secondServiceMock);
         String sofia = mainService.sayHello("Sofia");
-
         Assertions.assertEquals("Hello Sofia!", sofia);
     }
 
     @Test
     void sayGoodbye() {
-        SecondService secondServiceMock = mock(SecondService.class);
+        when(secondService.thirdMethod(anyString()))
+                .thenReturn("Goodbye Sofia!!!");
 
-        when(secondServiceMock.thirdMethod(anyString()))
-                .thenReturn("Goodbye Sofia!");
-
-        MainService mainService = new MainService(secondServiceMock);
         String sofia = mainService.sayGoodBye("Sofia");
-
-        Assertions.assertEquals("Goodbye Sofia!", sofia);
+        Assertions.assertEquals("Goodbye Sofia!!!", sofia);
     }
 }
